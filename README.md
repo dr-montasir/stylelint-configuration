99d47bda6fb3b0be42b044604eb2bb5fb08a9093
montasir <montasir1400@gmail.com>
2021-02-13 22:23:01 +0300

    version 1.0.0

---
+++ README.md
@@ -0,0 +1,11 @@
+### What is this?
+
+stylelint-configuration: Please ignore. This package does not ready yet.
+
+
+
+### Installation
+
+`npm i stylelint-configuration --save`
+
+
---
+++ index.js
@@ -0,0 +1,13 @@
+/*********************************************/
+/*  Author : Montasir Mirghani               */
+/*  Email  : contact@montasir.me             */
+/*  GitHub : https://github.com/dr-montasir  */
+/*********************************************/
+
+const useConfig = () => {
+	return console.log(`stylelint-configuration: Please ignore. This package does not ready yet.`);
+};
+
+const stylelintConfig = useConfig();
+
+module.exports.stylelintConfig = stylelintConfig;
